d35c06d3940f0e59c39d224d32900832d0e155b7
Bhupesh <bhupesh100@gmail.com>
2020-11-19 14:33:15 +0000

    Added interface.js and css files

---
+++ src/interface.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  $('#temperature').text(thermostat.temperature);
+
+
+  $('temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving').text('on')
+    updateTemperature();
+  });
+
+  $('powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving').text('off')
+    updateTemperature();
+  });
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  };
+  
+});
